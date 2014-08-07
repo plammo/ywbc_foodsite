a782301c6e8f4b58adffb892468f3b4208b76920
plammo <plammo@live.com>
2014-08-07 19:03:13 +1200

    latest changes and updates

--- includes/main_content.php
+++ includes/main_content.php
@@ -13,48 +13,73 @@
 					<img src="assets/images/slider_cake2.png" class="img-responsive" alt="pic" title="cake">
 					</div>
 				</div>
+
+				<div class="row row__mess">
+					<div class="col-xs-12">
+					<h1>You know you want to...</h1>
+					</div>
+				</div>
 				
 				<div class="row row__button row__3 row__color-1">
-					<div class="col-xs-12">
-					this is the opening hours
+					<div class="col-xs-12 butlab">
+						<a href"#">
+						<h1>Open</h1>
+						<h2>NOW!</h2></a>	
 					</div>
 				</div>
 				
 				<div class="row row__button row__4 row__color-2">
-					<div class="col-xs-12">
-					this is the menu
+					<div class="col-xs-12 butlab">
+						<a href"#" class="menu">
+							<h1>Cake</h1>
+							<h2>MENU</h2>
+						</a>
 					</div>
+
+					<div class="col-xs-12 expand exp_menu">
+					</div>
+
 				</div>
 				
-				<div class="row row__button .col-xs-6 row__5 mpics--left">
+				<div class="row row__5 mpics--left">
 					<div class="col-xs-6">
-					this is the menu images left
+					
 					</div>
 				</div>
 
-				<div class="row row__button .col-xs-6 row__5 mpics--right">
+				<div class="row row__5 mpics--right">
 					<div class="col-xs-6">
-					this is the menu images right
+					
 					</div>
 				</div>
 				
 				<div class="row row__button row__6 row__color-1">
-					<div class="col-xs-12">
-						<h1>Open</h1>
-						<h2>NOW!</h2>					
+					<div class="col-xs-12 butlab">
+						<h1>Nearest</h1>
+						<h2>CAKE.</h2>					
 					</div>
+
+					<div class="col-xs-12 expand exp_loc">
+					</div>
+					
 				</div>
 				
 				<div class="row row__button row__7 row__color-2">
-					<div class="col-xs-12">
-					this is the phone call
+					<div class="col-xs-12 butlab">
+						<h1>Call</h1>
+						<h2>CAKE.</h2>	
 					</div>
 				</div>
 				
 				<div class="row row__button row__8 row__color-1">
-					<div class="col-xs-12">
-					this is the social media
+					<div class="col-xs-12 butlab">
+						<h1>Get</h1>
+						<h2>SOCIAL</h2>	
 					</div>
+
+					<div class="col-xs-12 expand exp_smedia">
+					</div>
+					
 				</div>
 			
 
@@ -68,3 +93,24 @@
 	</div>
 
 </body>
+
+
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script>
+
+$( '.menu' ).click(function() {
+  $( '.row__4' ).slideToggle( "slow", function() {
+    // Animation complete.
+  });
+});
+
+ 
+ /*$(document).ready(function(){
+
+  $('.row__mess').fadeIn(17000);
+  
+});*/
+
+
+</script>
+
